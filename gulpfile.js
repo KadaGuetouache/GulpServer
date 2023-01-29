2a7bda90d821be0dc8e0f55e1d1ae4c2c9151fb1
KadaGuetouache <wail@disroot.org>
2023-01-29 11:03:34 +0100

    project test

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
 // Imports
-const { src, dest, series, watch } = require( 'gulp' );
-const sass = require( 'gulp-sass' )( require( 'sass' ) );
-const plumber = require( 'gulp-plumber' );
-const browserSync = require( 'browser-sync' ).create(  );
+const { src, dest, series, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+// const { default: postcss } = require('postcss');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss')
 
 const outputDir = './';
 
-const scssPages = [ "./scss/main.scss" ];
-const templates = [ "./index.html" ];
+const scssPages = ["./scss/main.css"];
+const templates = ["./index.html"];
 
 
 // Sass
 function styles(cb) {
-	src( scssPages, { allowEmpty: true } )
-		.pipe( sass(  ).on( 'error', sass.logError ) )
-		.pipe( dest( `${outputDir}/styles` ) )
-		.pipe( browserSync.stream() )
+	src(scssPages, { allowEmpty: true })
+		.pipe(sass().on('error', sass.logError))
+		// .pipe(postcss([
+		// 	require('tailwindcss')('./tailwind.config.js'),
+		// 	require('autoprefixer')
+		// ]))
+		.pipe(dest(`${outputDir}/styles`))
+		.pipe(browserSync.stream())
 
-		cb()
+	cb()
 }
 
 // Liver Server
@@ -27,22 +33,22 @@ function server(cb) {
 		open: true,
 		server: {
 			baseDir: `./`
-		}   
-})
+		}
+	})
 	cb();
 }
 
 
 function watcher(cb) {
-	watch( './scss/**/*.scss', styles )
-	watch( './scripts/*.js', reload )
-	watch( './*.html' ).on( 'change', reload )
+	watch('./scss/**/*.scss', styles)
+	watch('./scripts/*.js', reload)
+	watch('./*.html').on('change', reload)
 
 	cb();
 }
 
-function reload(){ 
+function reload() {
 	browserSync.reload()
 }
 
-exports.default = series( styles, server, watcher )
+exports.default = series(styles, server, watcher)
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        tranparent: 'transparent'
+        transparent: 'transparent',
       },
+      flex: {
+        '2': '2 2 0%',
+        '3': '3 3 0%',
+        '4': '4 4 0%'
+      },
+      aspectRatio: {
+        '16/9': '16 / 9',
+        '9/16': '9 / 16',
+      },
+      gridTemplateRows: {
+        'min': 'min-content'
+      }
+    },
+    transitionDelay: {
+      '400': '400ms',
+      '600': '600ms',
+      '800': '800ms',
+      '900': '900ms',
     },
   },
   plugins: [],
